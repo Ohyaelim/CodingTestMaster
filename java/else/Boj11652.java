d8acc458cf7973c67c0d91705084f16b95f4bd87
Yaelim <53201847+Ohyaelim@users.noreply.github.com>
2021-02-15 11:20:44 +0900

    Create Boj11652.java

---
+++ java/else/Boj11652.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        long arr[] = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int cnt=1,max=1;
+        long answer=arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if(arr[i]==arr[i-1]){
+                cnt++;
+            }else {
+                cnt = 1;
+            }
+            if(cnt>max){
+                max = cnt;
+                answer=arr[i];
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
